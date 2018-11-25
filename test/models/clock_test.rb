013c6f6035ab1120a68c5861c0ed34b7e072720a
hisayohorie <hisayohorie@gmail.com>
2018-11-25 16:26:05 -0500

    Adding tests

--- test/models/clock_test.rb
+++ test/models/clock_test.rb
@@ -1,7 +1,28 @@
 require 'test_helper'
 
 class ClockTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test 'valid clock' do
+    clock = Clock.new(user: 'Bobo', in_out: 'In', time: Time.now)
+    assert clock.valid?
+  end
+
+  test 'invalid clock without user' do
+    clock = Clock.new(in_out: 'In', time: Time.now)
+    refute clock.valid?
+  end
+
+  test 'invalid clock without In/Out' do
+    clock = Clock.new(user: 'Bobo', time: Time.now)
+    refute clock.valid?
+  end
+
+  test 'invalid clock without time' do
+    clock = Clock.new(user: 'Bobo', in_out: 'In')
+    refute clock.valid?
+  end
+
+  test 'invalid clock with wrong In/Out input' do
+    clock = Clock.new(user: 'Bobo', time: Time.now, in_out: 'Oops')
+    refute clock.valid?
+  end
 end
