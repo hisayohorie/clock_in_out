55a7c26c96f439cc73bdf46abcd77a2570ed0c61
hisayohorie <hisayohorie@gmail.com>
2018-11-25 15:32:22 -0500

    Wrapping timeCount function in if (currentTime) and using turbolinks:load

--- app/assets/javascripts/clocks.js
+++ app/assets/javascripts/clocks.js
@@ -1,18 +1,22 @@
-document.addEventListener('DOMContentLoaded', function(){
-  function timeCount() {
-      var today = new Date();
-      var h = today.getHours();
-      var m = today.getMinutes();
-      var s = today.getSeconds();
-      m = checkTime(m);
-      s = checkTime(s);
-      document.getElementById('currentTime').innerHTML =
-      h + ":" + m + ":" + s;
-      var t = setTimeout(timeCount, 500);
-  }
-  function checkTime(i) {
-      if (i < 10) {i = "0" + i};  // add zero in front of numbers < 10
-      return i;
-  }
- timeCount();
+document.addEventListener('turbolinks:load', function(){
+  var currentTime = document.getElementById('currentTime');
+
+  if(currentTime) {
+    function timeCount() {
+        var today = new Date();
+        var h = today.getHours();
+        var m = today.getMinutes();
+        var s = today.getSeconds();
+        m = checkTime(m);
+        s = checkTime(s);
+        currentTime.innerHTML = h + ":" + m + ":" + s;
+        var t = setTimeout(timeCount, 500);
+    }
+    function checkTime(i) {
+        if (i < 10) {i = "0" + i};
+        return i;
+    }
+   timeCount();
+ }
+
 });
